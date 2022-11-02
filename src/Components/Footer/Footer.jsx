a7986fd9b856a55a9ade2d11229b6fe457a95b74
olawale <jao.olaniyi@gmail.com>
2022-11-02 10:16:23 +0100

    footer in progress

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import { Col, Container, Row, Form, InputGroup } from 'react-bootstrap';
+import { Col, Container, Row, Form, InputGroup, Button } from 'react-bootstrap';
+import {BsFacebook, BsInstagram, BsLinkedin} from 'react-icons/bs';
+import {SiGmail} from 'react-icons/si';
+import {AiFillTwitterCircle} from 'react-icons/ai';
 import './Footer.scss'
 import WasteXpertLogo from '../../Assets/footerLogo.svg'
 
@@ -8,20 +11,68 @@ const Footer = () => {
     <div className='footer'>Footer
         <Container>
 <Row>
-    <Col md={8}>
+    <Col md={8} >
     <img src={WasteXpertLogo} alt="WasteXpertLogo" />
-    <p>Wastexpert</p>
+    <p style={{color:'#ffffff'}}>Wastexpert</p>
     </Col>
     <Col md={4}>
-    Subscribe to our Newsletter
+   <p style={{color:'#ffffff'}}> Subscribe to our Newsletter</p>
     <InputGroup className="mb-3">
         <Form.Control
-          placeholder="Enter your email address here"
-          aria-label="Enter your email address here"
-          aria-describedby="basic-addon2"
+        placeholder="Enter your email address here"
+        aria-label="Enter your email address here"
+        aria-describedby="basic-addon2"
         />
-        <InputGroup.Text id="basic-addon2">Sign up</InputGroup.Text>
+        <Button style={{backgroundColor:'#000000', color:'#ffffff', outline:'none' }} id="button-addon2">
+        Sign up
+        </Button>
       </InputGroup>
+    
+    </Col>
+</Row>
+<Row>
+    <Col md={5}>
+    <p>Wastexpert is a waste recycling and social benefit venture borne out of sheer passion and vision to transform our environment into a zero landfill  and zero waste nation. We operates an incentive-based scheme which collects recyclable materials from post consumers and in turn rewards them with “Points” which they can accumulate overtime and cash out or use to redeem/shop for household items offered through our Wastexpert store.</p>
+    </Col>
+    <Col md={3}>
+    <ul>
+        <li> <a href=""> About</a></li>
+        <li> <a href="">Our Mission</a></li>
+        <li> <a href="">Vision</a></li>
+        <li> <a href="">Solutions</a></li>
+        <li> <a href="">Teams</a></li>
+    </ul>
+    </Col>
+    <Col md={2}>
+    <ul>
+        <li> <a href=""> Contact Us</a></li>
+        <li> <a href="">Our Mission</a></li>
+        <li> <a href="">Vision</a></li>
+        <li> <a href="">Solutions</a></li>
+        <li> <a href="">Teams</a></li>
+    </ul>
+    </Col>
+    <Col md={2}>
+    <ul>
+        <li> <a href=""> Service</a></li>
+        <li> <a href="">Waste to wealth pick up</a></li>
+        <li> <a href="">Flip-it</a></li>
+        <li> <a href="">Waste drop off point</a></li>
+        <li> <a href="">Corporate Recycling</a></li>
+    </ul>
+    </Col>
+</Row>
+<hr />
+<Row>
+    <Col md={8}>
+        <b>Copyright © 2020, Created by Wastexpert Team</b>
+    </Col>
+    <Col md={4}>
+        <BsFacebook/>
+        <AiFillTwitterCircle/>
+        <BsInstagram/>
+        <BsLinkedin/>
+        <SiGmail/>
     </Col>
 </Row>
         </Container>
