417cbfab00f387133a45a88e243104db2543578a
olawale <jao.olaniyi@gmail.com>
2022-11-03 07:35:24 +0100

    roll with us continued

--- src/Components/RollWithUs/RollWithUs.jsx
+++ src/Components/RollWithUs/RollWithUs.jsx
@@ -8,26 +8,26 @@ import saveMoney from "../../Assets/save-money.svg";
 const RollWithUs = () => {
   return (
     <div className="rollwithus">
-      RollWithUs
+
       <Container>
-        <h2 style={{ textAlign: "center", color: "#000000" }}>
+        <h2 style={{ textAlign: "center", color: "#000000", paddingTop:'2rem' }}>
           Here is why You should roll with us
         </h2>
         <Row>
-          <Col md={4}>
-            <img src={saveMoney} alt="saveMoney" className="img-fluid text-align-center"/>
-            <p>Get paid on every kilogram of Waste collected</p>
+          <Col md={4} className='rollItems'>
+            <img src={saveMoney} alt="saveMoney" className="img-fluid pb-3"/>
+            <p className="rollP" >Get paid on every kilogram of Waste collected</p>
           </Col>
-          <Col md={4}>
-            <img src={truck} alt="truck" className="img-fluid" />
-            <p>
+          <Col md={4} className='rollItems'>
+            <img src={truck} alt="truck" className="img-fluid pb-3" />
+            <p className="rollP">
               Get a branded tricycle, IOT scale and all other neccessary tools
               to work with
             </p>
           </Col>
-          <Col md={4}>
-            <img src={settings} alt="settings" className="img-fluid" />
-            <p>
+          <Col md={4} className='rollItems'>
+            <img src={settings} alt="settings" className="img-fluid pb-3" />
+            <p className="rollP">
               Get train on how wastexpert platform works and how to calculate
               your earning
             </p>
